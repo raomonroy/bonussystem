a4877223638cb76bde90942f3dd9b012470ff09e
Ronny Angel Osca Monroy <ronny.osca@heptacom.de>
2026-04-23 11:52:04 +0200

    BS-1 Add Test and implementation
    Co-authored-by: Martin Sander <martin307@schule.bremen.de>

--- Bonusssystem/src/main/java/de/szut/Service/BonusService.java
+++ Bonusssystem/src/main/java/de/szut/Service/BonusService.java
@@ -1,1 +1,25 @@
+package de.szut.Service;
 
+public class BonusService {
+
+    private final double minBonus = 500.0;
+    private final double maxBonus = 10000.00;
+
+    public double applyCompanyRestrictions(double bonus) {
+        if (bonus > this.maxBonus) {
+            return this.maxBonus;
+        } else if (bonus < this.minBonus) {
+            return this.minBonus;
+        }
+
+        return bonus;
+    }
+
+    public double getMinBonus() {
+        return minBonus;
+    }
+
+    public double getMaxBonus() {
+        return maxBonus;
+    }
+}
--- Bonusssystem/src/test/java/de/szut/ServiceTest/BonusServiceTest.java
+++ Bonusssystem/src/test/java/de/szut/ServiceTest/BonusServiceTest.java
@@ -1,1 +1,26 @@
+package de.szut.ServiceTest;
 
+import de.szut.Service.BonusService;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class BonusServiceTest {
+
+    private final BonusService bonusService = new BonusService();
+
+    @Test
+    void returnBonusUnchangedWhenWithinLimits() {
+        assertThat(bonusService.applyCompanyRestrictions((4000.0))).isEqualTo(4000.0);
+    }
+
+    @Test
+    void capBonusAtMaximum() {
+        assertThat(bonusService.applyCompanyRestrictions((15000.0))).isEqualTo(10000.0);
+    }
+
+    @Test
+    void raiseBonusToMinimum() {
+        assertThat(bonusService.applyCompanyRestrictions((100.0))).isEqualTo(500.0);
+    }
+}
