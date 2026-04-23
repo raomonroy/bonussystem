2b9423ef175052429d5f053fc5deb76b41a32549
Ronny Angel Osca Monroy <ronny.osca@heptacom.de>
2026-04-23 14:34:43 +0200

    APD-2 Add AbsencePenaltyDecorator and deduct600ForMOreThan25AbsenceDaysTest
    Co-authored-by: Martin Sander <martinsander88@gmail.com>

--- Bonusssystem/src/test/java/de/szut/DecoratorTest/AbsencePenaltyDecoratorTest.java
+++ Bonusssystem/src/test/java/de/szut/DecoratorTest/AbsencePenaltyDecoratorTest.java
@@ -15,13 +15,23 @@ public class AbsencePenaltyDecoratorTest {
     BonusComponentInterface bonusComponent = mock(BonusComponentInterface.class);
 
     @Test
-    void deduct600ForMOreThan25AbsenceDays() {
+    void deduct600ForMoreThan25AbsenceDays() {
         Employee employee = new Employee("Rebecca", 3, 70, 0, 26, false);
         when(bonusComponent.calculateBonus(employee)).thenReturn(1000.0);
 
         BonusComponentInterface absencePenaltyDecorator = new AbsencePenaltyDecorator(bonusComponent);
 
         assertThat(absencePenaltyDecorator.calculateBonus(employee)).isEqualTo(400.0);
+    }
+
+    @Test
+    void deduct300ForMoreThan15AbsenceDays() {
+        Employee employee = new Employee("Rebecca", 3, 70, 0, 16, false);
+        when(bonusComponent.calculateBonus(employee)).thenReturn(1000.0);
+
+        BonusComponentInterface absencePenaltyDecorator = new AbsencePenaltyDecorator(bonusComponent);
+
+        assertThat(absencePenaltyDecorator.calculateBonus(employee)).isEqualTo(700.0);
     }
 
 }
